b18a98a70640269eb5cf898c2185c8d97a9b572d
Malek <zaag.malek1@gmail.com>
2023-11-11 21:10:01 +0100

    first commit

---
+++ src/main/java/com/example/tp4/Tp4Application.java
@@ -0,0 +1,12 @@
+package com.example.tp4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp4Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Tp4Application.class, args);
+		System.out.println("application working");
+	}
+}

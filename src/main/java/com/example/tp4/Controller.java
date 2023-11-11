296e9a6279f943335d7f0f726b07c5a23a91a7b4
Malek <zaag.malek1@gmail.com>
2023-11-11 22:40:17 +0100

    adding tests

--- src/main/java/com/example/tp4/Controller.java
+++ src/main/java/com/example/tp4/Controller.java
@@ -1,4 +1,31 @@
 package com.example.tp4;
 
+
+import lombok.Data;
+import netscape.javascript.JSObject;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Data
+@RestController
 public class Controller {
+
+    @GetMapping("/")
+    public Map<String, String> Geter(){
+        HashMap<String,String> map = new HashMap<>();
+        map.put("msg","hello, this is a GET request");
+        return map;
+
+    }
+    @PostMapping( "/")
+    public Map<String, String> Poster(){
+        HashMap<String,String> map = new HashMap<>();
+        map.put("msg","hello, this is a POST request");
+        return map;
+    }
+
 }
--- src/test/java/com/example/tp4/Tp4ApplicationTests.java
+++ src/test/java/com/example/tp4/Tp4ApplicationTests.java
@@ -1,13 +1,41 @@
 package com.example.tp4;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 class Tp4ApplicationTests {
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+	private MockMvc mockMvc;
 
-	@Test
-	void contextLoads() {
+	@BeforeEach
+	public void setup() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
 	}
 
+	@Test
+	public void GetReq() throws Exception {
+		mockMvc.perform(get("/")).andExpect(status().isOk())
+				.andExpect(content().contentType("application/json"))
+				.andExpect(jsonPath("msg").value("hello, this is a GET request"));
+
+	}
+	@Test
+	public void PostReq() throws Exception {
+		mockMvc.perform(post("/")).andExpect(status().isOk())
+				.andExpect(content().contentType("application/json"))
+				.andExpect(jsonPath("msg").value("hello, this is a POST request"));
+	}
 }

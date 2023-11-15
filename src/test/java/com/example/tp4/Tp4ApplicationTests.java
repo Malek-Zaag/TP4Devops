d9cac2e25ff08150c853ab03c7eb9ae566ba6b14
Malek <zaag.malek1@gmail.com>
2023-11-16 00:39:02 +0100

    .

--- src/test/java/com/example/tp4/Tp4ApplicationTests.java
+++ src/test/java/com/example/tp4/Tp4ApplicationTests.java
@@ -1,41 +1,26 @@
 package com.example.tp4;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@Data
+@AllArgsConstructor
 class Tp4ApplicationTests {
-	@Autowired
-	private WebApplicationContext webApplicationContext;
-	private MockMvc mockMvc;
+    //    @MockBean
+//    private final Student student;
+//    @InjectMocks
+//    private final StudentService studentService;
 
-	@BeforeEach
-	public void setup() {
-		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-	}
 
-	@Test
-	public void GetReq() throws Exception {
-		mockMvc.perform(get("/")).andExpect(status().isOk())
-				.andExpect(content().contentType("application/json"))
-				.andExpect(jsonPath("msg").value("hello, this is a GET request"));
+    @Test
+    public void test() {
 
-	}
-	@Test
-	public void PostReq() throws Exception {
-		mockMvc.perform(post("/")).andExpect(status().isOk())
-				.andExpect(content().contentType("application/json"))
-				.andExpect(jsonPath("msg").value("hello, this is a POST request"));
-	}
+        assertEquals("hello world", "t");
+    }
+
 }
